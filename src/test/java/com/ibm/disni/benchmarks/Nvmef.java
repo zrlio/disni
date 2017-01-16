7d1972f03844d026c87a95d5dec55b19e79af96b
Jonas Pfefferle <jpf@zurich.ibm.com>
2017-01-16 15:54:20 +0100

    nvmef: more generic benchmark functionality

--- src/test/java/com/ibm/disni/benchmarks/Nvmef.java
+++ src/test/java/com/ibm/disni/benchmarks/Nvmef.java
@@ -24,85 +24,94 @@ package com.ibm.disni.benchmarks;
 import com.ibm.disni.nvmef.spdk.*;
 import sun.nio.ch.DirectBuffer;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Nvmef {
 
-	public static void main(String[] args) throws Exception {
+	private final NvmeNamespace namespace;
+	private final NvmeQueuePair queuePair;
+
+	private final Random random;
+
+	final static int ITERS = 10000;
+
+	Nvmef(String address, String port, String subsystemNQN) throws IOException {
 		Nvme nvme = new Nvme();
 		NvmeTransportId tid = new NvmeTransportId(NvmeTransportType.RDMA, NvmfAddressFamily.IPV4, "10.40.0.17", "4420",
 				"nqn.2014-08.org.nvmexpress.discovery");
 		ArrayList<NvmeController> controllers = new ArrayList<NvmeController>();
 		nvme.probe(tid, controllers);
 		NvmeController controller = controllers.get(0);
-		NvmeNamespace namespace = controller.getNamespace(1);
-		NvmeQueuePair qpair = controller.allocQueuePair();
+		namespace = controller.getNamespace(1);
+		queuePair = controller.allocQueuePair();
 
-		Random rand = new Random(System.nanoTime());
-		// 128KB = maximum transfer size
-		ByteBuffer buffer = ByteBuffer.allocateDirect(1024*128);
+		random = new Random(System.nanoTime());
+	}
+
+	enum AccessPattern {
+		SEQUENTIAL,
+		RANDOM,
+		SAME
+	}
+
+	long run(int queueDepth, int transferSize, AccessPattern accessPattern, boolean write) throws IOException {
+		IOCompletion completions[] = new IOCompletion[queueDepth];
+		ByteBuffer buffer = ByteBuffer.allocateDirect(transferSize);
 		byte bytes[] = new byte[buffer.capacity()];
-		rand.nextBytes(bytes);
+		random.nextBytes(bytes);
 		buffer.put(bytes);
 
-		final int ITERS = 10000;
-		IOCompletion completion;
+		int sectorCount = transferSize / namespace.getSectorSize();
+
 		long start = System.nanoTime();
-		for (int i = 0; i < ITERS; i++) {
-			completion = namespace.read(qpair, ((DirectBuffer)buffer).address(), 100, 1);
-			do {
-				qpair.processCompletions(1);
-				completion.update();
-			} while (!completion.done());
+		for (int j = 0; j < ITERS; ) {
+			for (int i = 0; i < completions.length; i++) {
+				boolean post = false;
+				if (completions[i] == null) {
+					post = true;
+				} else {
+					completions[i].update();
+					if (completions[i].done()) {
+						post = true;
+					}
+				}
+
+				if (post) {
+					long lba = 0;
+					switch (accessPattern) {
+						case SEQUENTIAL:
+							lba = j * sectorCount;
+							break;
+						case RANDOM:
+							lba = random.nextInt(1024*1024);
+							break;
+						case SAME:
+							lba = 1024;
+							break;
+					}
+					if (write) {
+						completions[i] = namespace.write(queuePair, ((DirectBuffer)buffer).address(), lba, sectorCount);
+					} else {
+						completions[i] = namespace.read(queuePair, ((DirectBuffer)buffer).address(), lba, sectorCount);
+					}
+					j++;
+				}
+			}
+			queuePair.processCompletions(completions.length);
 		}
 		long end = System.nanoTime();
-		System.out.println("Read latency (same location) = " + (end - start)/ITERS + "ns");
+		return (end - start)/ITERS;
+	}
 
-		start = System.nanoTime();
-		for (int i = 0; i < ITERS; i++) {
-			completion = namespace.read(qpair, ((DirectBuffer)buffer).address(), rand.nextInt(1024*1024), 1);
-			do {
-				qpair.processCompletions(1);
-				completion.update();
-			} while (!completion.done());
+	public static void main(String[] args) throws Exception {
+		if (args.length < 3) {
+			System.out.println("<address> <port> <subsystemNQN>");
+			System.exit(-1);
 		}
-		end = System.nanoTime();
-		System.out.println("Read latency (random location) = " + (end - start)/ITERS + "ns");
-
-		start = System.nanoTime();
-		for (int i = 0; i < ITERS; i++) {
-			completion = namespace.write(qpair, ((DirectBuffer)buffer).address(), 1024, 1);
-			do {
-				qpair.processCompletions(1);
-				completion.update();
-			} while (!completion.done());
-		}
-		end = System.nanoTime();
-		System.out.println("Write latency (same location) = " + (end - start)/ITERS + "ns");
-
-		start = System.nanoTime();
-		for (int i = 0; i < ITERS; i++) {
-			completion = namespace.write(qpair, ((DirectBuffer)buffer).address(), rand.nextInt(1024*1024), 1);
-			do {
-				qpair.processCompletions(1);
-				completion.update();
-			} while (!completion.done());
-		}
-		end = System.nanoTime();
-		System.out.println("Write latency (random location) = " + (end - start)/ITERS + "ns");
-
-
-		start = System.nanoTime();
-		for (int i = 0; i < ITERS; i++) {
-			completion = namespace.read(qpair, ((DirectBuffer)buffer).address(), rand.nextInt(1024*1024), 262144);
-			do {
-				qpair.processCompletions(1);
-				completion.update();
-			} while (!completion.done());
-		}
-		end = System.nanoTime();
-		System.out.println("Read qd = 1 / 128KB (random location) = " + (end - start)/ITERS + "ns");
+		Nvmef nvmef = new Nvmef(args[0], args[1], args[2]);
+		System.out.println("Read latency (random) = " + nvmef.run(1, 512, AccessPattern.RANDOM, false) + "ns");
 	}
 }
