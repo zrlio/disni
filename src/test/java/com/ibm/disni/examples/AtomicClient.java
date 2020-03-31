0e7f40345f08a975ef8b16d4406496c8b7371c46
Konstantin Taranov <konstantin.taranov@inf.ethz.ch>
2020-03-31 20:45:28 +0200

    added support for atomics and made a simple test

---
+++ src/test/java/com/ibm/disni/examples/AtomicClient.java
@@ -0,0 +1,268 @@
+/*
+ * DiSNI: Direct Storage and Networking Interface
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016-2018, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.disni.examples;
+
+import com.ibm.disni.CmdLineCommon;
+import com.ibm.disni.verbs.*;
+import org.apache.commons.cli.ParseException;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.LinkedList;
+
+public class AtomicClient {
+	private String ipAddress;
+	private int port;
+
+	public void run() throws Exception {
+		System.out.println("AtomicClient::starting...");
+		//open the CM and the verbs interfaces
+
+		//create a communication channel for receiving CM events
+		RdmaEventChannel cmChannel = RdmaEventChannel.createEventChannel();
+		if (cmChannel == null){
+			System.out.println("AtomicClient::cmChannel null");
+			return;
+		}
+
+		//create a RdmaCmId for this client
+		RdmaCmId idPriv = cmChannel.createId(RdmaCm.RDMA_PS_TCP);
+		if (idPriv == null){
+			System.out.println("AtomicClient::id null");
+			return;
+		}
+
+		//before connecting, we have to resolve addresses
+		InetAddress _dst = InetAddress.getByName(ipAddress);
+		InetSocketAddress dst = new InetSocketAddress(_dst, port);
+		idPriv.resolveAddr(null, dst, 2000);
+
+		//resolve addr returns an event, we have to catch that event
+		RdmaCmEvent cmEvent = cmChannel.getCmEvent(-1);
+		if (cmEvent == null){
+			System.out.println("AtomicClient::cmEvent null");
+			return;
+		} else if (cmEvent.getEvent() != RdmaCmEvent.EventType.RDMA_CM_EVENT_ADDR_RESOLVED
+				.ordinal()) {
+			System.out.println("AtomicClient::wrong event received: " + cmEvent.getEvent());
+			return;
+		}
+		cmEvent.ackEvent();
+
+		//we also have to resolve the route
+		idPriv.resolveRoute(2000);
+		//and catch that event too
+		cmEvent = cmChannel.getCmEvent(-1);
+		if (cmEvent == null){
+			System.out.println("AtomicClient::cmEvent null");
+			return;
+		} else if (cmEvent.getEvent() != RdmaCmEvent.EventType.RDMA_CM_EVENT_ROUTE_RESOLVED
+				.ordinal()) {
+			System.out.println("AtomicClient::wrong event received: " + cmEvent.getEvent());
+			return;
+		}
+		cmEvent.ackEvent();
+
+		//let's create a device context
+		IbvContext context = idPriv.getVerbs();
+
+		//and a protection domain, we use that one later for registering memory
+		IbvPd pd = context.allocPd();
+		if (pd == null){
+			System.out.println("AtomicClient::pd null");
+			return;
+		}
+
+		//the comp channel is used for getting CQ events
+		IbvCompChannel compChannel = context.createCompChannel();
+		if (compChannel == null){
+			System.out.println("AtomicClient::compChannel null");
+			return;
+		}
+
+		//let's create a completion queue
+		IbvCQ cq = context.createCQ(compChannel, 50, 0);
+		if (cq == null){
+			System.out.println("AtomicClient::cq null");
+			return;
+		}
+		//and request to be notified for this queue
+		cq.reqNotification(false).execute().free();
+
+		//we prepare for the creation of a queue pair (QP)
+		IbvQPInitAttr attr = new IbvQPInitAttr();
+		attr.cap().setMax_recv_sge(1);
+		attr.cap().setMax_recv_wr(10);
+		attr.cap().setMax_send_sge(1);
+		attr.cap().setMax_send_wr(10);
+		attr.setQp_type(IbvQP.IBV_QPT_RC);
+		attr.setRecv_cq(cq);
+		attr.setSend_cq(cq);
+		//let's create a queue pair
+		IbvQP qp = idPriv.createQP(pd, attr);
+		if (qp == null){
+			System.out.println("AtomicClient::qp null");
+			return;
+		}
+
+		int buffercount = 3;
+		int buffersize = 100;
+		ByteBuffer buffers[] = new ByteBuffer[buffercount];
+		IbvMr mrlist[] = new IbvMr[buffercount];
+		int access = IbvMr.IBV_ACCESS_LOCAL_WRITE | IbvMr.IBV_ACCESS_REMOTE_WRITE | IbvMr.IBV_ACCESS_REMOTE_READ;
+
+		//before we connect we also want to register some buffers
+		for (int i = 0; i < buffercount; i++){
+			buffers[i] = ByteBuffer.allocateDirect(buffersize);
+			mrlist[i] = pd.regMr(buffers[i], access).execute().free().getMr();
+		}
+
+		ByteBuffer dataBuf = buffers[0];
+		IbvMr dataMr = mrlist[0];
+		IbvMr sendMr = mrlist[1];
+		ByteBuffer recvBuf = buffers[2];
+		IbvMr recvMr = mrlist[2];
+
+		LinkedList<IbvRecvWR> wrList_recv = new LinkedList<IbvRecvWR>();
+
+		IbvSge sgeRecv = new IbvSge();
+		sgeRecv.setAddr(recvMr.getAddr());
+		sgeRecv.setLength(recvMr.getLength());
+		sgeRecv.setLkey(recvMr.getLkey());
+		LinkedList<IbvSge> sgeListRecv = new LinkedList<IbvSge>();
+		sgeListRecv.add(sgeRecv);
+		IbvRecvWR recvWR = new IbvRecvWR();
+		recvWR.setSg_list(sgeListRecv);
+		recvWR.setWr_id(1000);
+		wrList_recv.add(recvWR);
+
+		//it's important to post those receive operations before connecting
+		//otherwise the server may issue a send operation and which cannot be received
+		//this class wraps soem of the RDMA data operations
+		VerbsTools commRdma = new VerbsTools(context, compChannel, qp, cq);
+		commRdma.initSGRecv(wrList_recv);
+
+		//now let's connect to the server
+		RdmaConnParam connParam = new RdmaConnParam();
+		connParam.setRetry_count((byte) 2);
+		connParam.setResponder_resources((byte) 1);
+		connParam.setInitiator_depth((byte) 1);
+		idPriv.connect(connParam);		
+
+ 
+		//wait until we are really connected
+		cmEvent = cmChannel.getCmEvent(-1);
+		if (cmEvent == null){
+			System.out.println("AtomicClient::cmEvent null");
+			return;
+		} else if (cmEvent.getEvent() != RdmaCmEvent.EventType.RDMA_CM_EVENT_ESTABLISHED
+				.ordinal()) {
+			System.out.println("AtomicClient::wrong event received: " + cmEvent.getEvent());
+			return;
+		}
+		cmEvent.ackEvent();
+
+		//let's wait for the first message to be received from the server
+		commRdma.completeSGRecv(wrList_recv, false);
+
+		//here we go, it contains the RDMA information of a remote buffer
+		recvBuf.clear();
+		long addr = recvBuf.getLong();
+		int length = recvBuf.getInt();
+		int lkey = recvBuf.getInt();
+		recvBuf.clear();
+		System.out.println("AtomicClient::receiving rdma information, addr " + addr + ", length " + length + ", key " + lkey);
+		System.out.println("AtomicClient::preparing atomic operation...");
+		
+		dataBuf.order(ByteOrder.LITTLE_ENDIAN);
+		System.out.println("AtomicClient::initial value in the buffer: " + dataBuf.getLong());
+
+		//let's prepare a one-sided RDMA read operation to fetch the content of that remote buffer
+		LinkedList<IbvSendWR> wrList_send = new LinkedList<IbvSendWR>();
+		IbvSge sgeSend = new IbvSge();
+		sgeSend.setAddr(dataMr.getAddr());
+		sgeSend.setLength(8);
+		sgeSend.setLkey(dataMr.getLkey());
+		LinkedList<IbvSge> sgeList = new LinkedList<IbvSge>();
+		sgeList.add(sgeSend);
+		IbvSendWR sendWR = new IbvSendWR();
+		sendWR.setWr_id(1001);
+		sendWR.setSg_list(sgeList);
+		sendWR.setOpcode(IbvSendWR.IBV_WR_ATOMIC_FETCH_AND_ADD);
+		sendWR.setSend_flags(IbvSendWR.IBV_SEND_SIGNALED);
+		sendWR.getAtomic().setRemote_addr(addr);
+		sendWR.getAtomic().setRkey(lkey);
+		sendWR.getAtomic().setCompare_add(10);
+		wrList_send.add(sendWR);
+
+		//now we post the operation, the RDMA/read operation will take off
+		//the wrapper class will also wait of the CQ event
+		//once the CQ event is received we know the RDMA/read operation has completed
+		//we should have the content of the remote buffer stored in our own local buffer
+		//let's print it
+		commRdma.send(buffers, wrList_send, true, false);
+		dataBuf.clear();
+		System.out.println("AtomicClient::read value from server: " + dataBuf.getLong());
+		
+		//now we send a final message to signal everything went fine
+		sgeSend = new IbvSge();
+		sgeSend.setAddr(sendMr.getAddr());
+		sgeSend.setLength(sendMr.getLength());
+		sgeSend.setLkey(sendMr.getLkey());
+		sgeList.clear();
+		sgeList.add(sgeSend);
+		sendWR = new IbvSendWR();
+		sendWR.setWr_id(1002);
+		sendWR.setSg_list(sgeList);
+		sendWR.setOpcode(IbvSendWR.IBV_WR_SEND);
+		sendWR.setSend_flags(IbvSendWR.IBV_SEND_SIGNALED);
+		wrList_send.clear();
+		wrList_send.add(sendWR);
+
+		//let's post the final message
+		commRdma.send(buffers, wrList_send, true, false);
+	}
+
+
+	public void launch(String[] args) throws Exception {
+		CmdLineCommon cmdLine = new CmdLineCommon("AtomicClient");
+
+		try {
+			cmdLine.parse(args);
+		} catch (ParseException e) {
+			cmdLine.printHelp();
+			System.exit(-1);
+		}
+		ipAddress = cmdLine.getIp();
+		port = cmdLine.getPort();
+
+		this.run();
+	}
+
+	public static void main(String[] args) throws Exception {
+		AtomicClient AtomicClient = new AtomicClient();
+		AtomicClient.launch(args);
+	}
+}
+
---
+++ src/test/java/com/ibm/disni/examples/AtomicServer.java
@@ -0,0 +1,245 @@
+/*
+ * DiSNI: Direct Storage and Networking Interface
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016-2018, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.disni.examples;
+
+import com.ibm.disni.CmdLineCommon;
+import com.ibm.disni.verbs.*;
+import org.apache.commons.cli.ParseException;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.LinkedList;
+
+public class AtomicServer {
+	private String ipAddress;
+	private int port;
+
+	public void run() throws Exception {
+		System.out.println("AtomicServer::starting...");
+
+		//create a communication channel for receiving CM events
+		RdmaEventChannel cmChannel = RdmaEventChannel.createEventChannel();
+		if (cmChannel == null){
+			System.out.println("AtomicServer::CM channel null");
+			return;
+		}
+
+		//create a RdmaCmId for the server
+		RdmaCmId idPriv = cmChannel.createId(RdmaCm.RDMA_PS_TCP);
+		if (idPriv == null){
+			System.out.println("idPriv null");
+			return;
+		}
+
+		InetAddress _src = InetAddress.getByName(ipAddress);
+		InetSocketAddress src = new InetSocketAddress(_src, port);
+		idPriv.bindAddr(src);
+
+		//listen on the id
+		idPriv.listen(10);
+
+		//wait for new connect requests
+		RdmaCmEvent cmEvent = cmChannel.getCmEvent(-1);
+		if (cmEvent == null){
+			System.out.println("cmEvent null");
+			return;
+		}
+		else if (cmEvent.getEvent() != RdmaCmEvent.EventType.RDMA_CM_EVENT_CONNECT_REQUEST
+				.ordinal()) {
+			System.out.println("AtomicServer::wrong event received: " + cmEvent.getEvent());
+			return;
+		}
+		//always acknowledge CM events
+		cmEvent.ackEvent();
+
+		//get the id of the newly connection
+		RdmaCmId connId = cmEvent.getConnIdPriv();
+		if (connId == null){
+			System.out.println("AtomicServer::connId null");
+			return;
+		}
+
+		//get the device context of the new connection, typically the same as with the server id
+		IbvContext context = connId.getVerbs();
+		if (context == null){
+			System.out.println("AtomicServer::context null");
+			return;
+		}
+
+		//create a new protection domain, we will use the pd later when registering memory
+		IbvPd pd = context.allocPd();
+		if (pd == null){
+			System.out.println("AtomicServer::pd null");
+			return;
+		}
+
+		//the comp channel is used to get CQ notifications
+		IbvCompChannel compChannel = context.createCompChannel();
+		if (compChannel == null){
+			System.out.println("AtomicServer::compChannel null");
+			return;
+		}
+
+		//create a completion queue
+		IbvCQ cq = context.createCQ(compChannel, 50, 0);
+		if (cq == null){
+			System.out.println("AtomicServer::cq null");
+			return;
+		}
+		//request to be notified on that CQ
+		cq.reqNotification(false).execute().free();
+
+		//prepare a new queue pair
+		IbvQPInitAttr attr = new IbvQPInitAttr();
+		attr.cap().setMax_recv_sge(1);
+		attr.cap().setMax_recv_wr(10);
+		attr.cap().setMax_send_sge(1);
+		attr.cap().setMax_send_wr(10);
+		attr.setQp_type(IbvQP.IBV_QPT_RC);
+		attr.setRecv_cq(cq);
+		attr.setSend_cq(cq);
+		//create the queue pair for the client connection
+		IbvQP qp = connId.createQP(pd, attr);
+		if (qp == null){
+			System.out.println("AtomicServer::qp null");
+			return;
+		}
+
+		int buffercount = 3;
+		int buffersize = 100;
+		ByteBuffer buffers[] = new ByteBuffer[buffercount];
+		IbvMr mrlist[] = new IbvMr[buffercount];
+		int access = IbvMr.IBV_ACCESS_LOCAL_WRITE | IbvMr.IBV_ACCESS_REMOTE_WRITE | IbvMr.IBV_ACCESS_REMOTE_READ | IbvMr.IBV_ACCESS_REMOTE_ATOMIC;
+
+		RdmaConnParam connParam = new RdmaConnParam();
+		connParam.setRetry_count((byte) 2);
+		connParam.setResponder_resources((byte) 1);
+		connParam.setInitiator_depth((byte) 1);
+		//once the client id is set up, accept the connection
+		connId.accept(connParam);
+		//wait until the connection is officially switched into established mode
+		cmEvent = cmChannel.getCmEvent(-1);
+		if (cmEvent.getEvent() != RdmaCmEvent.EventType.RDMA_CM_EVENT_ESTABLISHED
+				.ordinal()) {
+			System.out.println("AtomicServer::wrong event received: " + cmEvent.getEvent());
+			return;
+		}
+		//always ack CM events
+		cmEvent.ackEvent();
+
+		//register some buffers to be used later
+		for (int i = 0; i < buffercount; i++){
+			buffers[i] = ByteBuffer.allocateDirect(buffersize);
+			mrlist[i] = pd.regMr(buffers[i], access).execute().free().getMr();
+		}
+
+		ByteBuffer dataBuf = buffers[0];
+		IbvMr dataMr = mrlist[0];
+		ByteBuffer sendBuf = buffers[1];
+		IbvMr sendMr = mrlist[1];
+		IbvMr recvMr = mrlist[2];
+
+		dataBuf.order(ByteOrder.LITTLE_ENDIAN);
+		dataBuf.putLong(dataMr.getAddr());
+		dataBuf.clear();
+
+		System.out.println("AtomicServer::content of buffer before: " + dataBuf.getLong());
+		dataBuf.clear();
+
+		sendBuf.putLong(dataMr.getAddr());
+		sendBuf.putInt(dataMr.getLength());
+		sendBuf.putInt(dataMr.getLkey());
+		sendBuf.clear();
+
+		//this class is a thin wrapper over some of the data operations in jverbs
+		//we use it to issue data transfer operations
+		VerbsTools commRdma = new VerbsTools(context, compChannel, qp, cq);
+		LinkedList<IbvSendWR> wrList_send = new LinkedList<IbvSendWR>();
+
+		//let's prepare some work requests for sending
+		IbvSge sgeSend = new IbvSge();
+		sgeSend.setAddr(sendMr.getAddr());
+		sgeSend.setLength(sendMr.getLength());
+		sgeSend.setLkey(sendMr.getLkey());
+		LinkedList<IbvSge> sgeList = new LinkedList<IbvSge>();
+		sgeList.add(sgeSend);
+		IbvSendWR sendWR = new IbvSendWR();
+		sendWR.setWr_id(2000);
+		sendWR.setSg_list(sgeList);
+		sendWR.setOpcode(IbvSendWR.IBV_WR_SEND);
+		sendWR.setSend_flags(IbvSendWR.IBV_SEND_SIGNALED);
+		wrList_send.add(sendWR);
+
+		LinkedList<IbvRecvWR> wrList_recv = new LinkedList<IbvRecvWR>();
+
+		//let's prepare some work requests for receiving
+		IbvSge sgeRecv = new IbvSge();
+		sgeRecv.setAddr(recvMr.getAddr());
+		sgeRecv.setLength(recvMr.getLength());
+		int lkey = recvMr.getLkey();
+		sgeRecv.setLkey(lkey);
+		LinkedList<IbvSge> sgeListRecv = new LinkedList<IbvSge>();
+		sgeListRecv.add(sgeRecv);
+		IbvRecvWR recvWR = new IbvRecvWR();
+		recvWR.setSg_list(sgeListRecv);
+		recvWR.setWr_id(2001);
+		wrList_recv.add(recvWR);
+
+		//post a receive call
+		commRdma.initSGRecv(wrList_recv);
+		System.out.println("AtomicServer::initiated recv, about to send stag info");
+		//post a send call, here we send a message which include the RDMA information of a data buffer
+		commRdma.send(buffers, wrList_send, true, false);
+		System.out.println("AtomicServer::stag info sent");
+
+		//wait for the final message from the server
+		commRdma.completeSGRecv(wrList_recv, false);
+
+		System.out.println("AtomicServer::content of buffer after: " + dataBuf.getLong());
+		dataBuf.clear();
+
+		System.out.println("AtomicServer::done");
+	}
+
+	public void launch(String[] args) throws Exception {
+		CmdLineCommon cmdLine = new CmdLineCommon("AtomicServer");
+
+		try {
+			cmdLine.parse(args);
+		} catch (ParseException e) {
+			cmdLine.printHelp();
+			System.exit(-1);
+		}
+		ipAddress = cmdLine.getIp();
+		port = cmdLine.getPort();
+
+		this.run();
+	}
+
+	public static void main(String[] args) throws Exception {
+		AtomicServer AtomicServer = new AtomicServer();
+		AtomicServer.launch(args);
+	}
+}
+
